021ac23c124b5fac351811ac7fe0f94a12814be7
razaan24 <daffarazann@gmail.com>
2024-12-05 08:52:15 +0700

    p

--- controllers/controller.js
+++ controllers/controller.js
@@ -135,6 +135,7 @@ class Controller {
 
             
             res.render("home.ejs", { posts });
+            
         } catch (error) {
             console.log(error)
             res.send(error)
@@ -207,13 +208,50 @@ class Controller {
         }
     }
 
+    static async showEditProfile(req, res) { 
+        try {
+            const profile = await Profile.findOne({
+                where: {
+                    UserId: req.params.UserId
+                },
+                include: [
+                    {
+                        model: User,
+                        attributes: ['id', 'email'],
+                        include: [  // Nested includes
+                            {
+                                model: Profile,
+                                attributes: ['username', 'bio']
+                            }
+                        ]
+                    },
+                    {
+                        model: Post,
+                        attributes: ['caption', 'image']
+                    }
+                ]
+            });
+    
+            res.render("profile.ejs", { profile, posts: profile.Posts });
+        } catch (error) {
+            res.send (error)
+        }
+    }
+
+
+
+
+
+
     static async aboutUs(req, res) {
         try {
-            res.render("aboutUs.ejs")
+
+
         } catch (error) {
             res.send(error)
         }
     }
+
 }
 
 module.exports = Controller
